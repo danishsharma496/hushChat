079aa6359d45c780c7aa18f1b8b2da5b024955a4
danishsharma496 <danishsharma496@gmail.com>
2024-01-25 12:08:01 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,16 +2,29 @@
 // npm install @google/generative-ai express
 
 const express = require('express');
+const axios = require('axios');
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
-const dotenv = require('dotenv').config()
-
+const dotenv = require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 3000;
+
+
+const cors = require('cors');
+app.use(cors()); 
+
+
+const port = process.env.PORT || 3015;
+const API_KEY = process.env.API_KEY;
+const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
+
 app.use(express.json());
 const MODEL_NAME = "gemini-pro";
-const API_KEY = process.env.API_KEY;
+ 
 
-async function runChat(userInput) {
+let chat; // Declare chat globally
+
+function initializeChatModel() {
+  
+ 
   const genAI = new GoogleGenerativeAI(API_KEY);
   const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
@@ -30,55 +43,122 @@ async function runChat(userInput) {
     // ... other safety settings
   ];
 
-  const chat = model.startChat({
+  chat = model.startChat({
     generationConfig,
     safetySettings,
     history: [
+
       {
         role: "user",
-        parts: [{ text: "You are Sam, a friendly assistant who works for Coding Money. Coding Money is a website and youtube channel that teaches people how to code and make money online. Your job is to capture user's name and email address. Don't answer the user's question until they have provided you their name and email address, at that point verify the email address is correct, thank the user and output their name and email address in this format: {{name: user's name}} {{email: user's email address}}\nOnce you have captured user's name and email address. Answer user's questions related to Coding Money.\nCoding Money's website URL is: https://CodingMoney.com website is coming soon. Coding Money's Youtube Channel URL is: https://youtube.com/CodingMoney Coding Money's Facebook Page is: https://facebook.com/codingmoneycom Coding Money's Tiktok account is: https://tiktok.com/@codingmoneycom Coding Money's X formerly Twitter is: https://x.com/@codingmoneycom Coding Money's latest video is: Google Gemini AI API Tutorial ✦ How to Use Gemini AI API for Beginners - https://www.youtube.com/watch?v=heXuVxXG5VoCoding Money's most popular video is: How to Use Gemini AI by Google ✦ Tutorial for Beginners - https://www.youtube.com/watch?v=btPBE-fjHeg Coding Money's oldest video is: What is Coding Money? Top 3 Ways of Making Money with Coding - https://www.youtube.com/watch?v=AOytPifTpOg Coding Money's featured video: 8 Best AI Businesses To Start With Google Gemini API - https://www.youtube.com/watch?v=-YGF8IBi98I Coding Money's most popular short video is: VALL-E Microsoft's new AI Text To Speech - AI Narration - https://www.youtube.com/shorts/fPSOlZyTOJ4 Mukhtar is the founder of Coding Money. Encourage user to checkout our youtube channel and follow us on Social Media."}],
+        parts: [{ text: '  you are a Shopping  assistant your goal is to help  user choose desired  product after analysing  purchase history and user data after asking sufficient questions  PurchaseArrayDescriptions = [Product names, Brands, Models, Prices (in Rupees), Types, Purposes];    purchaseHistory = [["Sony Noise-Canceling Headphones", "Sony", "WH-1000XM4", 15000, "Electronics", "Audio Quality and Noise Isolation"],["Apple MacBook Pro", "Apple", "MacBook Pro 13-inch", 120000, "Electronics", "Productivity and Design"],["Nike Air Zoom Pegasus Running Shoes", "Nike", "Air Zoom Pegasus 38", 8000, "Footwear", "Athletic Performance"],["Amazon Echo Dot (4th Gen)", "Amazon", "Echo Dot (4th Gen)", 3000, "Electronics", "Convenience and Entertainment"],["Canon EOS Rebel T7i DSLR Camera", "Canon", "EOS Rebel T7i", 50000, "Electronics", "Photography"],["KitchenAid Stand Mixer", "KitchenAid", "Artisan Series", 25000, "Appliances", "Kitchen Convenience"]] '}],
       },
       {
         role: "model",
-        parts: [{ text: "Hello! Welcome to Coding Money. My name is Sam. What's your name?"}],
+        parts: [{ text: "can u give me user details "}],
       },
+
       {
         role: "user",
-        parts: [{ text: "Hi"}],
+        parts: [{ text: '  UserData={name:"Danish Sharma"  , height:"6feet" , age:"21" ,  weight:"85kg" , body:"Muscular lean body " ,activities:"gym , calisthenics, sports , college , software Engineer", preferences:"plain colors with less graphics"  , size:{shoes:9 , upper:"XL" , waist,"32" , chest"44" ,FavouriteBrand:"Nike , Fossil , RedTape" }}' ,}],
       },
       {
         role: "model",
-        parts: [{ text: "Hi there! Thanks for reaching out to Coding Money. Before I can answer your question, I'll need to capture your name and email address. Can you please provide that information?"}],
+        parts: [{ text: "I  will ask u small question and strictly recomend one product (may recomend anpther if user not satisfied ) enclose product name  in {{product}} curly brackets  and all responses from me would be short and precise "}],
       },
     ],
   });
-
-  const result = await chat.sendMessage(userInput);
-  const response = result.response;
-  return response.text();
+ 
 }
 
+
+
+
+
+
+
+
+
+
+
+async function searchAmazonData(query) {
+  const options = {
+    method: 'GET',
+    url: 'https://real-time-amazon-data.p.rapidapi.com/search',
+    params: {
+      query: query,
+      page: '1',
+      country: 'IN',
+      sort_by: 'RELEVANCE',
+      category_id: 'aps',
+      // min_price: minPrice.toString(),
+      // max_price: maxPrice.toString()
+    },
+    headers: {
+      'X-RapidAPI-Key': '188fbcb24dmsha2a0ae5a1906c8ep1ca78fjsnb2f852efc33f',
+      'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    // console.log(response.data.data.products[0]);
+    const item = response.data.data.products[0] ; 
+
+    return item;  
+  } catch (error) {
+    console.error(error);
+    throw error; // You might want to handle the error or rethrow it as needed
+  }
+}
+
+// Initialize chat model before any routes
+
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  initializeChatModel();
+  console.log("new chat");
+  res.status(200).send("Success");
 });
-app.get('/loader.gif', (req, res) => {
-  res.sendFile(__dirname + '/loader.gif');
-});
+
+
 app.post('/chat', async (req, res) => {
   try {
     const userInput = req.body?.userInput;
-    console.log('incoming /chat req', userInput)
+    console.log('incoming /chat req', userInput);
     if (!userInput) {
       return res.status(400).json({ error: 'Invalid request body' });
     }
 
-    const response = await runChat(userInput);
-    res.json({ response });
+    const result = await chat.sendMessage(userInput);
+    const response = result.response.text();
+    console.log(response);
+    res.json({ response: response  });
   } catch (error) {
     console.error('Error in chat endpoint:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+
+app.post('/product', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /product req', userInput);
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const result = await searchAmazonData(userInput);
+   
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
